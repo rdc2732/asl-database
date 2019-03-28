06ba93448af76558e2d1bd319c11cbf52319ca75
Rick Cottle <richard.cottle@honeywell.com>
2019-03-28 14:32:30 -0700

    Working version: read master_list.xlsx and cleanup Product columns.

--- make_db.py
+++ make_db.py
@@ -1,6 +1,16 @@
 # make_db.py
 # Parse the asl spreadsheets and create a relational database containing the same data
 
-import xlrd
 import sqlite3
+import numpy as np
+import pandas as pd
+
+excel_file = 'master_list.xlsx'
+
+asl_data = pd.read_excel(excel_file)
+
+asl_data.replace({'(blank)': np.NaN, ' - PL':'',' - PAC':'', ' - PT':''}, inplace=True, regex=True)
+
+print(asl_data[['ProductType','ProductLine','ProductGroup','ProductFamily']].head())
+
 
